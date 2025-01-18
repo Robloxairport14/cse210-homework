42a28d847e5329efe92c3e0592612f81dfd67d79
Dylan Scott Durtschi <dur21020@byui.edu>
2025-01-17 20:37:26 -0700

    parts 1 and 2 over due

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your percentage in your grade?");
+        
+        string number = Console.ReadLine();
+        int grade = int.Parse(number);   
+  
+        if (grade >= 90 && grade <= 100)
+        {
+            Console.WriteLine("You got an 'A'");
+        }
+        else if (grade >= 80 && grade <= 89)
+        {
+            Console.WriteLine("You got a 'B'");
+        }
+        else if (grade >= 70 && grade <= 79)
+        {
+            Console.WriteLine("You got a 'C'");
+        }
+        else if (grade >= 60 && grade <= 69)
+        {
+            Console.WriteLine("You got a 'D'");
+        }
+        else
+        {
+            Console.WriteLine("You got an 'F'");
+        }
     }
 }
