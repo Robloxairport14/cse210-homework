1fa85ce4570aac8422ebcb78b924bf6a21d9c407
Dylan Scott Durtschi <dur21020@byui.edu>
2025-01-17 17:33:07 -0700

    I am so late and hope I did this right.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        
+        string adjective = GetAdjective();
+        string noun = GetNoun();
+        int number = Multiply(3, 4);
+        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}");
+        //or {GetAdjective} in {adjective}
     }
+
+    static int Multiply (int number1, int number2)
+    {
+        int product = number1 * number2;
+
+        return product;
+    }
+    static string GetAdjective()
+    {
+
+        List<string> words = new List<string>();
+        words.Add("red");
+        words.Add("big");
+        words.Add("small");
+        words.Add("orange");
+        words.Add("blue");
+        words.Add("black");
+        words.Add("funny");
+
+        string adjective = words [2];
+
+        return adjective;
+    }
+
+    static string GetNoun()
+    {
+        string noun ="boat";
+
+        return noun;
+    }
+
 }
