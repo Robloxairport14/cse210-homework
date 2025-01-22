12d47796dd898ec7431f414c0100b3f315323727
Dylan Scott Durtschi <dur21020@byui.edu>
2025-01-21 20:17:18 -0700

    notes from 1/21/25

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,41 +5,58 @@ class Program
     static void Main(string[] args)
     {
         
-        string adjective = GetAdjective();
-        string noun = GetNoun();
-        int number = Multiply(3, 4);
-        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}");
-        //or {GetAdjective} in {adjective}
+        // string adjective = GetAdjective();
+        // string noun = GetNoun();
+        // int number = Multiply(3, 4);
+        // Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}");
+        // //or {GetAdjective} in {adjective}
+    
+
+        // static int Multiply (int number1, int number2)
+        // {
+        //     int product = number1 * number2;
+
+        //     return product;
+        // }
+        // static string GetAdjective()
+        // {
+
+        //     List<string> words = new List<string>();
+        //     words.Add("red");
+        //     words.Add("big");
+        //     words.Add("small");
+        //     words.Add("orange");
+        //     words.Add("blue");
+        //     words.Add("black");
+        //     words.Add("funny");
+
+        //     string adjective = words [2];
+
+        //     return adjective;
+        // }
+
+        // static string GetNoun()
+        // {
+        //     string noun ="boat";
+
+        //     return noun;
+        // }
+
+
+        Movie FavoriteMovie = new Movie();
+        FavoriteMovie._title= "Star Wars";
+        FavoriteMovie._year = 1977;
+        FavoriteMovie._rating = "PG";
+        FavoriteMovie._runtime = 150;
+
+        FavoriteMovie.Display();
+
+        DisplayMovie(FavoriteMovie);
+        
+        static void DisplayMovie(Movie aMovie)
+        {
+            Console.WriteLine($"{aMovie._title} - {aMovie._year}");
+        }
+
     }
-
-    static int Multiply (int number1, int number2)
-    {
-        int product = number1 * number2;
-
-        return product;
-    }
-    static string GetAdjective()
-    {
-
-        List<string> words = new List<string>();
-        words.Add("red");
-        words.Add("big");
-        words.Add("small");
-        words.Add("orange");
-        words.Add("blue");
-        words.Add("black");
-        words.Add("funny");
-
-        string adjective = words [2];
-
-        return adjective;
-    }
-
-    static string GetNoun()
-    {
-        string noun ="boat";
-
-        return noun;
-    }
-
 }
